d85f2da280b80af4227fd0d6e6c988b1c0a4a6c5
Abhilasha Srivastava <106274670+abhilashasrivastava@users.noreply.github.com>
2024-01-03 23:38:31 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,35 @@
+def sume(x,y):
+    s=x+y
+    return s
+x=int(input("enter"))
+y=int(input("entet"))
+print(sume (x,y))
+#
+def sume(x,y):
+    s=x+y
+    print(s)
+x=int(input("entrr"))
+y=int(input("nttr"))
+sume(x,y)
+#
+i="454"
+print(int(i))
+#u=54
+#rint(str("u"))
+u="2.3"
+print(float(u))
+#
+x=eval("45"+"56")
+print(x)
+b=eval("45+51")
+print(b)
+j=("45+54")
+print(j)
+y="4"+"2"
+print(y)
+#
+for i in range(0,52):
+    print(i)
+#
+import math
+print(help(math.sqrt))
