a86afaf0150a6feef4880560f5ec302de9a0038f
Abhilasha Srivastava <106274670+abhilashasrivastava@users.noreply.github.com>
2024-01-08 22:35:12 +0530

    Add files via upload

---
+++ tuple.py
@@ -0,0 +1,19 @@
+#tuple functuion
+#len()
+t=(1,2,3,4,5,6)
+print(len(t))
+#
+t=(1,2,3,4,5,6)
+print(max(t))
+#
+t=(1,2,3,4,5,6)
+print(min(t))
+#
+t=(1,2,3,4,5,6)
+print(t.index(5))
+#
+t=(1,2,3,4,5,6)
+print(t.count(1))
+#
+t=tuple([1,2,3,4,5,6])
+print(t)
