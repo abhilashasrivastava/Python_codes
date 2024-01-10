e1f0a9c7afbf227436f32a66626ad7bcdefc9784
Abhilasha Srivastava <106274670+abhilashasrivastava@users.noreply.github.com>
2024-01-10 21:52:31 +0530

    Add files via upload

---
+++ month number and print its name.py
@@ -0,0 +1,23 @@
+n=int(input("enter a number"))
+if n==1:
+    print("january")
+elif n==2:
+    print("february")
+elif n==3:
+    print("march")
+elif n==4:
+    print("april")
+elif n==5:
+    print("june")
+elif n==6:
+     print("july")
+elif n==7:
+     print("august")
+elif n==8:
+    print("september")
+elif n==10:
+    print("october")
+elif n==11:
+    print("november")
+elif n==12:
+    print("december")
