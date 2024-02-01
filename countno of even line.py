7614250e4d3497755f93c32e08a0acd59175e396
Abhilasha Srivastava <106274670+abhilashasrivastava@users.noreply.github.com>
2024-02-01 23:10:08 +0530

    Add files via upload

---
+++ countno of even line.py
@@ -0,0 +1,10 @@
+def display_evenline():
+    myfile=open("E:\\Abhilasha\\abhi.txt","r")
+    count=0
+    for line in myfile:
+        count+=1
+        if count%2==0:
+            print("no of line:",count)
+    myfile.close()
+display_evenline()
+
